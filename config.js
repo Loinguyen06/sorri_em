00a45c445ebe43e50856a6cb9eb2328d343a1f1c
Lợi Nguyễn <loinguyen@asia.icsc.com>
2025-04-17 16:04:09 +0700

    Up

---
+++ config.js
@@ -0,0 +1,24 @@
+const CONFIG = {
+    introTitle: 'Chào em!',
+    introDesc: `Ngày hôm nay của em thế nào rồi nhỉ?
+    Anh biết em đang buồn
+    Đừng buồn nữa nhé em
+    Anh sẽ kể cho em một câu chuyện
+    Về một chàng trai...
+    Anh ấy muốn làm một điều gì đó, 
+    anh ấy cứ làm xong rồi cứ bị fail, 
+    đừng buồn anh ấy nhé.
+    `,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đang giận anh hay là đang bực nhưng mà trong lòng thì đang vuiiiiii phải khum 😙',
+    desc: 'Cho anh ấy xin lỗi em nhé !!!',
+    btnYes: 'Tất nhiên là như vậy <33',
+    btnNo: 'Không, không được chọn',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu người hay tạo bug như anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
+    mess: 'Anh biết mà 🥰😘😘',
+    messDesc: 'Tối nay 19:30h anh qua đón nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://facebook.com/loinguyen049' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
